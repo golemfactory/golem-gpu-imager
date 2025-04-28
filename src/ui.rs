2a34640221a83286a81eacccd3940d1fcf5f6182
Przemysław K. Rekucki <prekucki@rcl.pl>
2025-04-28 13:22:14 +0200

    Move ui/mod.rs to ui.rs for flatter module structure

--- src/ui.rs
+++ src/ui.rs
@@ -1,10 +1,10 @@
-mod start_screen;
-mod flash;
-mod edit;
+pub mod start_screen;
+pub mod flash;
+pub mod edit;
 
 pub use start_screen::view_start_screen;
 pub use flash::{view_select_os_image, view_configure_settings, view_select_target_device, view_writing_process, view_flash_completion};
 pub use edit::{view_select_existing_device, view_edit_configuration, view_edit_completion};
 
 // Include the logo SVG data
-pub const LOGO_SVG: &[u8] = include_bytes!("../assets/logo.svg");
+pub const LOGO_SVG: &[u8] = include_bytes!("assets/logo.svg");
